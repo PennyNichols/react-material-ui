ef5d57d0878d33c1e06b6b5891003eeefafa26e6
pnichols789 <pnichols789@gmail.com>
2022-10-15 15:57:17 -0500

    -

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,3 +1,4 @@
+import ResponsiveAppBar from './components/mui-comp/ResponsiveAppBar';
 import Button from './components/styled-comp/Button';
 import Container from './components/styled-comp/Container';
 import Header from './components/styled-comp/Header';
@@ -5,11 +6,14 @@ import Link from './components/styled-comp/Link';
 
 function App() {
   return (
-    <Container>
-      <Header>Hello World</Header>
-      <Button>Click Me!</Button>
-      <Link>This is a link</Link>
-    </Container>
+    <>
+      <Container>
+        <Header>Hello World</Header>
+        <Button>Click Me!</Button>
+        <Link>This is a link</Link>
+      </Container>
+      <ResponsiveAppBar/>
+    </>
   );
 }
 
