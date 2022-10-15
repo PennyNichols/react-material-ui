39a8aff6281e55785275cd26d5a5ef39479eb06b
pnichols789 <pnichols789@gmail.com>
2022-10-15 14:46:01 -0500

    -

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,12 +1,14 @@
 import Button from './components/styled-comp/Button';
 import Container from './components/styled-comp/Container';
 import Header from './components/styled-comp/Header';
+import Link from './components/styled-comp/Link';
 
 function App() {
   return (
     <Container>
       <Header>Hello World</Header>
       <Button>Click Me!</Button>
+      <Link>This is a link</Link>
     </Container>
   );
 }
